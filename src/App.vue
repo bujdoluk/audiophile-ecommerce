8097094a7109aa1c081bded25776974e9a05183e
Lukáš Bujdoš <lukas.bujdos@gmail.com>
2021-11-25 16:02:55 +0100

    update 0.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,131 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app">
+    <router-view />
+    <Home />
   </div>
-  <router-view/>
 </template>
 
+<script>
+export default {};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url("https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700&display=swap");
+// Reset
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Manrope", sans-serif;
+}
+.app {
+  width: 100vw;
+  background-color: #f2f2f2;
 }
 
-#nav {
-  padding: 30px;
+.flex {
+  display: flex;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.flex-column {
+  flex-direction: column;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.flex-row {
+  flex-direction: row;
+}
+
+.black {
+  color: #000000;
+}
+
+.black-light {
+  color: #101010;
+}
+
+.white {
+  color: #ffffff;
+}
+
+.grey-dark {
+  color: #f1f1f1;
+}
+
+.grey-light {
+  color: #fafafa;
+}
+
+.orange-light {
+  color: #fbaf85;
+}
+
+.orange-dark {
+  color: #d87d4a;
+}
+
+.btn-orange {
+  background-color: #d87d4a;
+  color: white;
+  width: 160px;
+  height: 48px;
+  text-transform: uppercase;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.btn-orange:hover {
+  background-color: #fbaf85;
+}
+
+.btn-white {
+  background-color: transparent;
+  color: #000000;
+  width: 160px;
+  height: 48px;
+  text-transform: uppercase;
+  border: 1px solid #000000;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.btn-white:hover {
+  background-color: #000000;
+  color: #ffffff;
+}
+
+.btn-black {
+  background-color: #000000;
+  color: #ffffff;
+  width: 160px;
+  height: 48px;
+  text-transform: uppercase;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.btn-black:hover {
+  background-color: #ffffff;
+  color: #000000;
+}
+
+.btn-shop {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 13px;
+  width: 58px;
+  height: 18px;
+  color: #000000;
+  opacity: 0.5;
+  text-transform: uppercase;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+}
+
+.btn-shop:hover {
+  color: #d87d4a;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,281 @@
-<template>
+9<template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="content">
+      <Navbar />
+      <div class="content-details flex flex-row">
+        <div class="left flex flex-column">
+          <div class="new">new product</div>
+          <div class="name">XX99 mark II headphones</div>
+          <div class="text">
+            Experience natural, lifelike audio adn exceptional build quality
+            made for the passionate music enthusiast.
+          </div>
+          <div>
+            <button class="btn-orange">See Product</button>
+          </div>
+        </div>
+        <div class="right">
+          <img src="../assets/home/desktop/headphone.png" alt="hero" />
+        </div>
+      </div>
+    </div>
+    <Categories />
+    <section class="speaker flex flex-row">
+      <div class="speaker-left">
+        <img
+          class="img-speaker"
+          src="../assets/home/desktop/image-speaker-zx9.png"
+          alt="speaker"
+        />
+      </div>
+      <div class="speaker-right flex flex-column">
+        <div class="name">ZX9 Speaker</div>
+        <div class="text">
+          Upgrade to premium speakers that are phenomenally built to deliver
+          truly remarkable sound.
+        </div>
+        <div>
+          <button class="btn-black">SEE product</button>
+        </div>
+      </div>
+      <div></div>
+    </section>
+    <section class="speaker-2 flex">
+      <div class="speaker-2-content flex flex-column">
+        <div class="name">ZX7 speaker</div>
+        <div>
+          <button class="btn-white">see product</button>
+        </div>
+      </div>
+    </section>
+    <section class="speaker-3 flex flex row">
+      <div class="speaker-3-left">
+        <img
+          class="img"
+          src="../assets/home/desktop/image-earphones-yx1.jpg"
+          alt="earphones"
+        />
+      </div>
+      <div class="speaker-3-right flex flex-column">
+        <div class="name">YX1 earphones</div>
+        <div>
+          <button class="btn-white">see product</button>
+        </div>
+      </div>
+    </section>
+    <section class="gear flex flex-row">
+      <div class="gear-left flex flex-column">
+        <div class="name">bringing you the <span>best</span> audio gear</div>
+        <div class="text">
+          Located at the heart of New York City, Audiophile is the premier store
+          for high end headphones, earphones, speakers and audio accessories. We
+          have a large showroom and luxury demonstration rooms available for you
+          to browse and experience a wide range of our products. Stop by our
+          store to meet some of the fantastic people who make Audiophile the
+          best place to buy your portable audio equipment.
+        </div>
+      </div>
+      <div class="gear-right">
+        <img
+          class="img"
+          src="../assets/shared/desktop/image-best-gear.jpg"
+          alt="gear"
+        />
+      </div>
+    </section>
+    <Footer />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Navbar from "../components/Navbar.vue";
+import Categories from "../components/Categories.vue";
+import Footer from "../components/Footer.vue";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  components: { Navbar, Categories, Footer },
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  background: #0e0e0e;
+  height: 729px;
+  margin: 0 auto;
+  z-index: 1;
+
+  .content {
+    width: 1440px;
+    color: #ffffff;
+    z-index: 2;
+    margin: 0 auto;
+
+    &-details {
+      .left {
+        flex-basis: 50%;
+        gap: 24px;
+        justify-content: center;
+        .new {
+          text-transform: uppercase;
+          opacity: 0.5;
+          font-size: 14px;
+          line-height: 19px;
+          letter-spacing: 10px;
+        }
+
+        .name {
+          text-transform: uppercase;
+          font-weight: bold;
+          font-size: 56px;
+        }
+        .text {
+          font-style: normal;
+          font-weight: 500;
+          font-size: 15px;
+          opacity: 0.5;
+        }
+      }
+    }
+
+    .right {
+      flex-basis: 50%;
+    }
+  }
+
+  .speaker {
+    background: #d87d4a;
+    border-radius: 8px;
+    width: 1110px;
+    height: 560px;
+    margin: 0 auto;
+    color: #ffffff;
+    overflow: hidden;
+    background-image: url(../assets/home/desktop/pattern-circles.svg);
+    background-repeat: no-repeat;
+    background-position-x: -90%;
+
+    .speaker-left {
+      flex-basis: 50%;
+      position: relative;
+
+      .img-speaker {
+        position: absolute;
+        bottom: -10px;
+        left: 120px;
+        width: 370px;
+        height: 453px;
+      }
+    }
+
+    .speaker-right {
+      flex-basis: 50%;
+      justify-content: center;
+      padding: 0 95px;
+      gap: 24px;
+
+      .name {
+        text-transform: uppercase;
+        font-weight: bold;
+        font-size: 56px;
+      }
+
+      .text {
+        font-style: normal;
+        font-weight: 500;
+        font-size: 15px;
+        opacity: 0.5;
+      }
+    }
+  }
+
+  .speaker-2 {
+    background-color: #979797;
+    border-radius: 8px;
+    width: 1110px;
+    height: 320px;
+    margin: 48px auto;
+    overflow: hidden;
+    background-image: url(../assets/home/desktop/image-speaker-zx7.jpg);
+    background-repeat: no-repeat;
+    background-position-x: -90%;
+    align-items: center;
+
+    .speaker-2-content {
+      align-items: flex-start;
+      margin-left: 95px;
+      gap: 32px;
+
+      .name {
+        font-weight: bold;
+        font-size: 28px;
+        line-height: 38px;
+        letter-spacing: 2px;
+        text-transform: uppercase;
+      }
+    }
+  }
+
+  .speaker-3 {
+    width: 1110px;
+    height: 320px;
+    margin: 0 auto;
+    gap: 30px;
+    .speaker-3-left {
+      border-radius: 8px;
+
+      .img {
+        border-radius: 8px;
+      }
+    }
+
+    .speaker-3-right {
+      background: #f1f1f1;
+      border-radius: 8px;
+      flex-basis: 50%;
+      padding-left: 95px;
+      justify-content: center;
+      gap: 32px;
+
+      .name {
+        font-weight: bold;
+        font-size: 28px;
+        line-height: 38px;
+        letter-spacing: 2px;
+        text-transform: uppercase;
+      }
+    }
+  }
+
+  .gear {
+    width: 1110px;
+    height: 588px;
+    margin: 200px auto;
+
+    .gear-left {
+      flex-basis: 50%;
+      gap: 32px;
+      justify-content: center;
+      margin-right: 125px;
+      .name {
+        font-style: normal;
+        font-weight: bold;
+        font-size: 40px;
+        line-height: 44px;
+        letter-spacing: 1.42857px;
+        text-transform: uppercase;
+
+        span {
+          color: #d87d4a;
+        }
+      }
+    }
+
+    .gear-right {
+      flex-basis: 50%;
+      .img {
+        border-radius: 8px;
+      }
+    }
   }
 }
-</script>
+</style>
